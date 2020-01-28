9981bedbd34b2ab0a883438de8fd7756c75becfe
jwlee64 <josiahwlee@gmail.com>
2020-01-28 11:29:15 -0800

    shiyou part3

--- source/main.c
+++ source/main.c
@@ -1,93 +1,162 @@
-/*	Author: josiahlee
- *  Partner(s) Name: Shiyou Wang
- *	Lab Section:
- *	Assignment: Lab #6  Exercise 1
- *	Exercise Description: 
- *
- *  Create a synchSM to blink three LEDs connected to ​PB0​, ​PB1​, and ​PB2 in sequence, 
- *  1 second each. Implement that synchSM in C using the method defined in class. 
- *  In addition to demoing your program, you will need to show that your code adheres 
- *  entirely to the method with no variations.
+/*	Author: Shiyou Wang swang324
+ *  Partner(s) Name: Josiah Lee
+ *	Lab Section: 
+ *	Assignment: Lab #6  Exercise #3
+ *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
+
+
+unsigned char curStage=0x00;
+unsigned char btns = 0x00;
+unsigned char temp=0x00;
+
+enum _state {INIT,BEFORE_PLUS_ONE, PLUS_ONE, BEFORE_MINUS_ONE, MINUS_ONE,RESET,STANDBY, LONG_PRESSED} state, restoreState;
+
 
 volatile unsigned char TimerFlag = 0;
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
 
-enum States { start, B0, B1, B2, lock, lock_up } state;
+unsigned long _avr_timer_M = 1; // Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Ticks
 
-void TimerOn() {
-	TCCR1B = 0x0B;
-	OCR1A = 125;
-	TIMSK1 = 0x02;
-	TCNT1 = 0;
-	SREG |- 0x80;
+void TimerOn(){
+TCCR1B = 0x0B; // Clear timer and set basic unit.    bit3---clear  bit2bit1bit0---time unit options   clock /64  125,000 ticks/s	
+OCR1A = 125;   // Set unit period.           num * ticks/s  = interupt time 
+TIMSK1 = 0x02; // Enable timer	
+
+TCNT1=0;  // Initial counter
+_avr_timer_cntcurr = _avr_timer_M; // TimerISR period in milliseconds
+
+SREG |= 0x80;  //Enable global interrupts
 }
 
-void TimerOff() {
-	TCCR1B = 1;
+void TimerOff(){	
+TCCR1B = 0x00; // bit3bit1bit0=000: timer off
 }
 
-void TimerISR() {
-	TimerFlag = 1;
+void TimerISR(){
+TimerFlag = 1;
 }
 
-ISR(TIMER1_COMPA_vect) {
-	_avr_timer_cntcurr--;
-	if (_avr_timer_cntcurr == 0){
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
+// TCNT1 == OCR1
+ISR(TIMER1_COMPA_vect){
+_avr_timer_cntcurr--;
+if (_avr_timer_cntcurr == 0){	
+TimerISR();
+_avr_timer_cntcurr = _avr_timer_M;
+}
 }
 
-void TimerSet(unsigned long M) {
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M){
+_avr_timer_M = M;
+_avr_timer_cntcurr = _avr_timer_M;
 }
 
-void tick() {
-	switch(state){
-		case start: state = B0; break;
-		case B0: if (~PINA & 0x01) state = lock; else state = B1; break;
-		case B1: if (~PINA & 0x01) state = lock; else state = B2; break;
-		case B2: if (~PINA & 0x01) state = lock; else state = B0; break;
-		case lock: if (PINA & 0x01) state = lock_up; break;
-		case lock_up: if (~PINA & 0x01) state = B0; break;
-		default: state = start; break;
-	}
 
-	switch(state){
-		case start: PORTB = 0x00; break;
-		case B0: PORTB = 0x01; break;
-		case B1: PORTB = 0x02; break;
-		case B2: PORTB = 0x04; break;
-		case lock: break;
-		case lock_up: break;
-		default: state = start; break;
-	}
+void Tick(){
 
+btns = PINA & 0x03;
+
+switch(state){
+
+case INIT:{
+temp = 0x07;
+if(btns==0x01)
+state = BEFORE_PLUS_ONE;
+else if(btns==0x02)
+state = BEFORE_MINUS_ONE;
+else if(btns==0x03)
+state = RESET;
+break;
+}
+case BEFORE_PLUS_ONE:	{
+if(btns==0x00)
+state = PLUS_ONE;
+else{	
+restoreState = PLUS_ONE;
+state = LONG_PRESSED;
+}	
+break;
+}
+case PLUS_ONE:{
+
+if(temp >= 0x09)
+temp = 0x09;
+else
+temp = temp + 0x01;
+
+state = STANDBY;
+
+break;
+}
+case BEFORE_MINUS_ONE:	{
+if(btns==0x00)
+state = MINUS_ONE;
+break;
+
+case MINUS_ONE:
+if(temp <= 0x00)
+temp = 0x00;
+else
+temp = temp - 0x01;
+state = STANDBY;
+break;
+}
+case STANDBY:{
+if(btns == 0x01)
+state = BEFORE_PLUS_ONE;
+else if(btns==0x02)
+state = BEFORE_MINUS_ONE;
+else if(btns == 0x03)
+state = RESET;
+break;
+{
+case RESET:{
+if(btns == 0x03)
+temp = 0x07;
+else
+state = INIT;
+break;
+}
+case LONG_PRESSED:{
+
+curStage++;
+if(curStage==10){
+curStage=0;
+state = restoreState;
+}
+else  {
+state  = LONG_PRESSED;
 }
 
-void main(void) {
-    /* Insert DDR and PORT initializations */
-	DDRB = 0x00; PORTB = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-	TimerSet(300);
-	TimerOn();
-	state = start;
-    /* Insert your solution below */
-    while (1) {
-    	tick();
-    	while(!TimerFlag);
-    	TimerFlag = 0;
-    }
-    return 1;
+break;
+}
+default:
+break;
+}
+
+PORTC = temp;
+
+}
+}
+}
+
+int main(void)
+{
+DDRA = 0x00;PORTA = 0xFF;
+DDRC = 0xFF;PORTC = 0x00;
+TimerSet(100); // 100ms
+TimerOn();
+state = INIT;
+while(1) {
+Tick();
+while (!TimerFlag);
+TimerFlag = 0;
+}
 }
